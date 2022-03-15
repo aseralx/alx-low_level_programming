fdd2b02f6339043bdb7a71a723ddd9f804964bb6
aseralx <aserosoftware@gmail.com>
2022-03-15 13:34:23 -0700

    dfdf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * main function doesn't return any value
- * Return: On success 1.
+ * Return: Always 0 (success)
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
