ddc8d735609aa4d15d2e529a0282e42cf9b53709
aseralx <aserosoftware@gmail.com>
2022-03-17 16:17:32 -0700

    alx_project

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   * _isupper - Check if a letter is upper
@@ -7,16 +6,12 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
-
-int _isupper(int c)
+int _isupper(int x)
 {
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
 
-    if(isupper(c))
-    {
-       return(1);
-    }
-    else
-    {
-        return(0);
-    }
+	return (0);
 }
