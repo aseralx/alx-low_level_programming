6931efca3a9641f4bf9c39e5a0758348298b06f6
aseralx <aserosoftware@gmail.com>
2022-03-17 17:30:46 -0700

    alx_project

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_numbers - check the code
+ *
+ *  Return: always 0.
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	_putchar('\n');
